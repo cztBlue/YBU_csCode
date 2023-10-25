efe53a36bec0d2ab8d5d721a2c651676b979e8d9
cztBlue <2691169154@qq.com>
2023-10-25 11:30:09 +0800

    添加实验4

---
+++ lesson4/code1.cpp
@@ -0,0 +1,52 @@
+# include<iostream>
+using namespace std;
+
+class Student
+{
+   private:
+     string name;
+     string no;
+     string sex;
+     string phone;
+   public:
+     void setName(string str) {
+        name = str;
+     }
+
+     void setNo(string str) {
+        no = str;
+     }  
+
+     void setSex(string str) {
+        sex = str;
+     }
+
+     void setPhone(string str) {
+        phone = str;
+     }
+
+     void printInfo() {
+        cout<<"姓名："<<name<<endl;
+        cout<<"学号："<<no<<endl;
+        cout<<"性别："<<sex<<endl;
+        cout<<"电话："<<phone<<endl;
+     }
+};
+
+
+int main()
+{
+    //分配内存
+    Student* stu = new Student;
+    stu->setName("张三");
+    stu->setNo("123456");
+    stu->setSex("男");
+    stu->setPhone("11111111111");
+    stu->printInfo();
+
+    //释放内存
+    delete stu;
+
+
+    return 0;
+}
